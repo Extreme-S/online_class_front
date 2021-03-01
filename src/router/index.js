17d09ea65c0eae072b2ea832b6e0c663cd488d4a
Extreme-S <1716224950@qq.com>
2021-03-01 08:41:39 +0800

    first

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,55 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'home',
+    component: () => import('../views/Home/Home.vue'),
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/order',
+    name: 'Order',
+    component: () => import('../views/Order/Order.vue'),
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/personal',
+    name: 'Personal',
+    component: () => import('../views/Personal/Personal.vue'),
+    meta: { requiresAuth: true },
+  },
+  //课程详情页面
+  {
+    path: '/coursedetail',
+    name: 'detail',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import('../views/CourseDetail/CourseDetail.vue'),
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login/Login.vue'),
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('../views/Register/Register.vue'),
+  },
+  {
+    path: '/pay',
+    name: 'Pay',
+    component: () => import('../views/Pay/Pay.vue'),
+    meta: { requiresAuth: true },
+  },
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
 })
 
 export default router
